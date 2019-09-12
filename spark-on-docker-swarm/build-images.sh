34dd5fbcb9477439dc7e90656833062ad44512da
Michael Kamprath <michael@kamprath.net>
2019-09-12 00:27:34 -0700

    updated registry location

--- spark-on-docker-swarm/build-images.sh
+++ spark-on-docker-swarm/build-images.sh
@@ -7,9 +7,9 @@ docker build -t configured-spark-node:latest ./configured-spark-node
 docker build -t spark-jupyter-notebook:latest ./spark-jupyter-notebook
 
 # tag image with local repository
-docker tag configured-spark-node:latest 10.1.1.1:5000/configured-spark-node:latest
-docker tag spark-jupyter-notebook:latest 10.1.1.1:5000/spark-jupyter-notebook:latest
+docker tag configured-spark-node:latest master:5000/configured-spark-node:latest
+docker tag spark-jupyter-notebook:latest master:5000/spark-jupyter-notebook:latest
 
 # push the images to local repository
-docker push 10.1.1.1:5000/configured-spark-node:latest
-docker push 10.1.1.1:5000/spark-jupyter-notebook:latest
+docker push master:5000/configured-spark-node:latest
+docker push master:5000/spark-jupyter-notebook:latest
