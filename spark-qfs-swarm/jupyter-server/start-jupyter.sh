d1c1eae8fd0b2b9c6a7e4f3544685f2debf6d590
Michael Kamprath <michael@kamprath.net>
2019-12-30 23:08:52 -0800

    updated jupyter notebook
    Updated Jyputer Notebook to a newer version and added Jupyter extensions.
    Enabled by default extensions for Table of Contents, code folding, and
    execution time tracking.

--- spark-qfs-swarm/jupyter-server/start-jupyter.sh
+++ spark-qfs-swarm/jupyter-server/start-jupyter.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-SHELL=/bin/bash XDG_RUNTIME_DIR=/home/spark/jupyter/runtime PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777 --notebook-dir=/home/spark/jupyter/notebooks --ip=* --no-browser --allow-root --NotebookApp.token='' --NotebookApp.password=''" $SPARK_HOME/bin/pyspark --packages graphframes:graphframes:0.7.0-spark2.4-s_2.11 --master spark://spark-master:7077
+SHELL=/bin/bash XDG_RUNTIME_DIR=/home/spark/jupyter/runtime PYSPARK_DRIVER_PYTHON=jupyter PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777 --notebook-dir=/home/spark/jupyter/notebooks --ip=0.0.0.0 --NotebookApp.password='' --NotebookApp.token=''" $SPARK_HOME/bin/pyspark --packages graphframes:graphframes:0.7.0-spark2.4-s_2.11 --master spark://spark-master:7077
