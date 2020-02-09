9e10e00ecd0fe09b217c7e7b59983e5d2d18cadd
Michael Kamprath <michael@kamprath.net>
2020-02-08 21:21:46 -0800

    updated qfs configuration
    Updated the QFS configuration by consolidating its configurations to spark-defaults.conf
    and make 2x replication the default for files created on QFS. Also make the build-images.sh
    script able to take arguments to be passed to the docker file builds, and made the
    worker-node Dockerfile configurable through ARG parameters. The intent here is to be able
    to build the images with different versions of key software.

--- spark-qfs-swarm/build-images.sh
+++ spark-qfs-swarm/build-images.sh
@@ -2,11 +2,28 @@
 
 set -e
 
+DOCKER_BUILD_ARGS=
+
+while getopts b: option
+do
+case "${option}"
+in
+b) DOCKER_BUILD_ARGS=${OPTARG};;
+esac
+done
+
+if [ -z "$DOCKER_ARGS" ]
+then
+	echo "Building with default docker optins"
+else
+	echo "Building with docker arguments = '$DOCKER_BUILD_ARGS'"
+fi
+
 # build images
-docker build -t worker-node:latest ./worker-node
-docker build -t qfs-master:latest ./qfs-master
-docker build -t spark-master:latest ./spark-master
-docker build -t jupyter-server:latest ./jupyter-server
+docker build -t worker-node:latest $DOCKER_BUILD_ARGS ./worker-node
+docker build -t qfs-master:latest $DOCKER_BUILD_ARGS ./qfs-master
+docker build -t spark-master:latest $DOCKER_BUILD_ARGS ./spark-master
+docker build -t jupyter-server:latest $DOCKER_BUILD_ARGS ./jupyter-server
 
 # tag image with local repository
 docker tag worker-node:latest master:5000/worker-node:latest
