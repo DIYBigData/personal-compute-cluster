0a45507fa2bd942ca7cedb1d38253a7a31af5c5e
Michael Kamprath <michael@kamprath.net>
2020-02-13 01:37:07 -0800

    corrected path bug

--- spark-qfs-swarm/qfs-master/start-qfs-master.sh
+++ spark-qfs-swarm/qfs-master/start-qfs-master.sh
@@ -4,7 +4,7 @@ $QFS_HOME/bin/metaserver $QFS_HOME/conf/Metaserver.prp &> $QFS_LOGS_DIR/metaserv
 
 python2 $QFS_HOME/webui/qfsstatus.py $QFS_HOME/conf/webUI.cfg &> $QFS_LOGS_DIR/webui.log &
 
-$QFS_HOME/bin/qfs -fs qfs://qfs-master:20000 -D fs.trash.minPathDepth=2 -runEmptier &
+$QFS_HOME/bin/tools/qfs -fs qfs://qfs-master:20000 -D fs.trash.minPathDepth=2 -runEmptier &
 
 # now do nothing and do not exit
 while true; do sleep 3600; done
