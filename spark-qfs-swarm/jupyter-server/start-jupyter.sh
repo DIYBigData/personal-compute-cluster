069946fda1bcbd9aa7e97f4018aa8c45e33debb7
Michael Kamprath <michael@kamprath.net>
2020-06-22 19:36:50 -0700

    updated to spark 3.0.0

--- spark-qfs-swarm/jupyter-server/start-jupyter.sh
+++ spark-qfs-swarm/jupyter-server/start-jupyter.sh
@@ -4,4 +4,4 @@ SHELL=/bin/bash \
     XDG_RUNTIME_DIR=/home/spark/jupyter/runtime \
     PYSPARK_DRIVER_PYTHON=jupyter \
     PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777 --notebook-dir=/home/spark/jupyter/notebooks --ip=0.0.0.0 --NotebookApp.password='' --NotebookApp.token=''" \
-    $SPARK_HOME/bin/pyspark --packages graphframes:graphframes:0.8.0-spark2.4-s_2.11 --master spark://spark-master:7077
+    $SPARK_HOME/bin/pyspark --packages graphframes:graphframes:0.8.0-spark3.0-s_2.12 --master spark://spark-master:7077
