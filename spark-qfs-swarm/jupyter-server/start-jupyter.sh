0a2c693b0c119ca399588ff494ea8bec1ae0b462
Michael Kamprath <michael@kamprath.net>
2021-05-17 04:50:59 +0000

    set package repository
    Given the demise of Bintray, we now need to specificy the packages repository. See: https://spark.apache.org/news/new-repository-service.html

--- spark-qfs-swarm/jupyter-server/start-jupyter.sh
+++ spark-qfs-swarm/jupyter-server/start-jupyter.sh
@@ -4,4 +4,7 @@ SHELL=/bin/bash \
     XDG_RUNTIME_DIR=/home/spark/jupyter/runtime \
     PYSPARK_DRIVER_PYTHON=jupyter \
     PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777 --notebook-dir=/home/spark/jupyter/notebooks --ip=0.0.0.0 --NotebookApp.password='' --NotebookApp.token=''" \
-    $SPARK_HOME/bin/pyspark --packages graphframes:graphframes:0.8.1-spark3.0-s_2.12 --master spark://spark-master:7077
+    $SPARK_HOME/bin/pyspark \
+    --packages graphframes:graphframes:$GRAPHFRAMES_VERSION \
+    --repositories https://repos.spark-packages.org/ \
+    --master spark://spark-master:7077
