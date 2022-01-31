f43e20d83e404ae656d5858c33f31b32b2b6c9d3
Michael Kamprath <michael@kamprath.net>
2022-01-31 06:56:02 +0000

    updated spark version and other tweaks

--- spark-qfs-swarm/jupyter-server/start-jupyter.sh
+++ spark-qfs-swarm/jupyter-server/start-jupyter.sh
@@ -3,7 +3,7 @@
 SHELL=/bin/bash \
     XDG_RUNTIME_DIR=/home/spark/jupyter/runtime \
     PYSPARK_DRIVER_PYTHON=jupyter \
-    PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777 --notebook-dir=/home/spark/jupyter/notebooks --ip=0.0.0.0 --NotebookApp.password='' --NotebookApp.token=''" \
+    PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser --port=7777 --notebook-dir=/home/spark/jupyter/notebooks --ip=0.0.0.0 --NotebookApp.password='' --NotebookApp.token='' --NotebookApp.iopub_data_rate_limit=1.0e10" \
     $SPARK_HOME/bin/pyspark \
     --packages graphframes:graphframes:$GRAPHFRAMES_VERSION \
     --repositories https://repos.spark-packages.org/ \
