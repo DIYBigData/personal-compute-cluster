9eeaf6ee974d3593fa2afafb06a2e9ec2a62a4db
Michael Kamprath <michael@kamprath.net>
2022-10-05 20:45:25 +0000

    updated to debian 10 and java 11

--- spark-qfs-swarm/qfs-master/start-qfs-master.sh
+++ spark-qfs-swarm/qfs-master/start-qfs-master.sh
@@ -2,7 +2,7 @@
 
 $QFS_HOME/bin/metaserver $QFS_HOME/conf/Metaserver.prp &> $QFS_LOGS_DIR/metaserver.log &
 
-python2 $QFS_HOME/webui/qfsstatus.py $QFS_HOME/conf/webUI.cfg &> $QFS_LOGS_DIR/webui.log &
+python2.7 $QFS_HOME/webui/qfsstatus.py $QFS_HOME/conf/webUI.cfg &> $QFS_LOGS_DIR/webui.log &
 
 $QFS_HOME/bin/tools/qfs -fs qfs://qfs-master:20000 -D fs.trash.minPathDepth=2 -runEmptier &
 
